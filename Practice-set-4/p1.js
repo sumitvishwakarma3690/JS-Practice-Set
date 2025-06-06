c38f587b47923713cdff01e99f1855c02c084719
Sumit Vishwakarma <162994296+sumitvishwakarma3690@users.noreply.github.com>
2025-06-06 21:18:56 +0530

    set 4 finish

---
+++ Practice-set-4/p1.js
@@ -0,0 +1,14 @@
+// Program to covert given string into lowerCase or Uppercase
+let str = "Sumit"
+let str2 = "MOBILE"
+
+function upperCase() {
+    let up = str.toUpperCase()
+    console.log(up)
+}
+function lowerCase() {
+    let lw = str2.toLowerCase()
+    console.log(lw)
+}
+upperCase()
+lowerCase()
---
+++ Practice-set-4/scipt.js
@@ -0,0 +1,20 @@
+//1
+console.log("sum\"".length) //4
+
+//2
+let name = "Sumit"
+function check() {
+   let arr = name.startsWith("Su") 
+   console.log(arr)
+}
+check()
+
+//4
+let amt = "Please Give Rs 1000"
+let newAmt = amt.slice(15)
+console.log(newAmt)
+
+//5
+let chr = "Device"
+let newChr =  chr.replace("c", "d")
+console.log(newChr)
